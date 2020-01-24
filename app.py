fe5954e6e70b76ac1931749b6ad55516253bb551
Blueeagle <Blueeagle@balloucollin@gmail.com>
2020-01-24 13:26:29 -0700

    I've added blocks and a layout file

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Collin Ballou',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January, 24 2020'
+    },
+    {
+        'author': 'Collin Ballou',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January, 24 2020'
+    }
+]
 
 @app.route("/")
 @app.route('/home')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
